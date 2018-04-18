937fab0574a3ce4611293d1cd8f44ea821ec2050
yangzhaobin <yangzb0922@163.com>
2018-04-18 16:02:15 +0800

    fix

--- middlewares/register_spider.js
+++ middlewares/register_spider.js
@@ -4,11 +4,12 @@
  * @Author: yangzhaobin 
  * @Date: 2018-04-02 13:52:57 
  * @Last Modified by: yangzhaobin
- * @Last Modified time: 2018-04-02 17:43:02
+ * @Last Modified time: 2018-04-18 16:01:49
  */
 const Spider = require('../lib/spider');
 
 module.exports = (ctx) => {
     ctx.spider = ctx.spider || new Spider(ctx.appName);
+    console.info('spider', ctx.spider);
     ctx.spider.init().start();
 };
