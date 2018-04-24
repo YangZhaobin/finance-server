09ffffb6e99a6ec88adba88a300689c1fe801ae9
yangzhaobin <yangzb0922@163.com>
2018-04-24 20:33:14 +0800

    fix

--- controllers/artical.js
+++ controllers/artical.js
@@ -1,8 +1,11 @@
+const config = require('../const/web_const');
 const Artical = require('../db/model/artical');
 const CrawlerNetease = require('../crawler/artical/netease');
 const CrawlerPeople = require('../crawler/artical/people');
 const CrawlerTencent = require('../crawler/artical/tencent');
 const CrawlerPrcfe = require('../crawler/artical/prcfe');
+const CrawlerWallstreet = require('../crawler/artical/wallstreet');
+const Crawler10jqka = require('../crawler/artical/10jqka');
 const Website = require('../db/model/website');
 
 exports.getArticalById = async(ctx, next) => {
@@ -19,8 +22,12 @@ exports.getArticalById = async(ctx, next) => {
         artical = await CrawlerNetease.crawlArtical(data.url);
     } else if (data.website_id === 4) {
         artical = await CrawlerPeople.crawlArtical(data.url);
+    } else if (data.website_id === 5) {
+        artical = await CrawlerWallstreet.crawlArtical(data.url);
     } else if (data.website_id === 6) {
         artical = await CrawlerPrcfe.crawlArtical(data.url);
+    } else if (data.website_id === 7) {
+        artical = await Crawler10jqka.crawlArtical(data.url);
     }
     ctx.body = artical;
     return next();
@@ -44,28 +51,7 @@ exports.getAllArticalsByWeb = async(ctx, next) => {
     let params = ctx.request.query;
     let { site, type = '', limit = 10, offset = 0} = params;
     let data = {};
-    let site_id = 1;
-    switch(site) {
-        case 'sina':
-            site_id = 1;
-            break;
-        case 'tencent':
-            site_id = 2;
-            break;
-        case 'netease':
-            site_id = 3;
-            break;
-        case 'people':
-            site_id = 4;
-            break;
-        case 'wallstreet':
-            site_id = 5;
-            break;
-        case 'prcfe':
-            site_id = 6;
-            break;
-    }
-
+    let site_id = config[site].id;
     data = await Artical.findAllArticalsByWeb(site_id, type, limit, offset);
     ctx.body = data;
     return next();
